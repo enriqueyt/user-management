22f39a16c000e36a989077760226205d0cdbe3e8
Aaron Contreras <aaroncontreras1990@gmail.com>
2024-11-10 17:34:28 -0400

    Set Up of Redux User Slice

---
+++ packages/frontend/src/redux/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './userSlice';
+
+export const store = configureStore({
+  reducer:   
+ {
+    users: userReducer,
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
