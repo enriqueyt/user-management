ef43a771c2708a9c82536ff99b2711e1f2eb8859
Enrique Yepez Torres <enriqueyepez@Enriques-iMac.local>
2024-11-09 23:44:38 -0500

    stabish connection with MongoDB

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user.module';
+import { DataSourceModule } from './data-source/data-source.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [DataSourceModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
